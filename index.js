0f1e3f916ff536117877ff1cc1801b0e9462c762
shashank patel <shashank.patel@moduscreate.com>
2022-08-27 16:48:51 +0530

    add envs

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const axios = require('axios')
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const API_URL = core.getInput('devenv-api');
+  const CLIENTSECRETKEYID = core.getInput('client-secret-key-id');
+  const CLIENTSECRETKEY = core.getInput('client-secret-key');
   console.log(`Hello ${nameToGreet}!`);
+  console.log("*****************",API_URL,CLIENTSECRETKEYID,CLIENTSECRETKEY)
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload)
   console.log(`The event payload: ${payload}`);
-  axios.post('http://ptsv2.com/t/devenv/', payload)
+  axios.post(API_URL, payload)
   .then(function (response) {
     console.log(response);
   })
