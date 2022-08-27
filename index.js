804dc686090d5917a61e0bedfd0f55432dc9fbe3
shashank patel <shashank.patel@moduscreate.com>
2022-08-27 17:14:42 +0530

    remove logs

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ try {
   const API_URL = core.getInput('devenv-api-url');
   const CLIENTSECRETKEYID = core.getInput('devenv-client-secret-key-id');
   const CLIENTSECRETKEY = core.getInput('devenv-client-secret-key');
-  console.log("*****************",API_URL,CLIENTSECRETKEYID,CLIENTSECRETKEY)
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload)
   console.log(`The event payload: ${payload}`);
   axios.post(API_URL, payload)
